3e7a77d718251ff59f88a128d6f1b33dd3a6cfb6
Brandon Haley <brandon.haley94@gmail.com>
2013-12-16 12:44:04 -0800

    Removed all trace of SDL

--- src/HumanView.h
+++ src/HumanView.h
@@ -5,18 +5,12 @@
 #include <boost/archive/binary_iarchive.hpp>
 #include <boost/make_shared.hpp>
 #include <fstream>
-#include "Interfaces.h"
 #include "Logging.h"
 #include "globals.h"
-#include "ViewSystem.h"
 #include "GameEvents.h"
-#include "TestObject.h"
-#include "Map.h"
+#include "IEventListener.h"
 #include <map>
-#include <Gui.h>
 #include <vector>
-#include <SDL\SDL.h>
-#include <SDL\SDL_ttf.h>
 
 
 enum VirtualKeys
@@ -30,39 +24,17 @@ enum VirtualKeys
 
 //Purpose: To handle all human interaction with the system. it's only interaction with the system must be firing events and reciving events
 
-class HumanView : public IGameView, public IEventListener
+class HumanView : public IEventListener
 {
     public:
-        HumanView();
+        HumanView(std::string playerName);
         ~HumanView();
         bool init();
         void tick();
         bool handleEvent(Event const & event);
     private:
-
-        //typedefs
-
-        typedef std::vector<IScreenElementPtr> IScreenElementMap;
-        typedef std::pair<uint32_t, IScreenElementPtr> IScreenElementEnt;
-        typedef std::pair<IScreenElementMap::iterator, bool> IScreenElementRes;
-
-        typedef std::map<SDLKey, VirtualKeys> VirtualKeyboard;
-        typedef std::pair<SDLKey, VirtualKeys> VirtualKeyboardEnt;
-        typedef std::pair<VirtualKeyboard::iterator, bool> VirtualKeyboardRes;
-
-        //member variables
-
-        SDL_Surface *screen, *buf, *test;
-        IScreenElementMap *renderableObjects;
-        VirtualKeyboard keys;
+        std::string plName;
         Logging log;
-        SDL_Rect offset, vec, mapRect;
-        SDL_Event event;
-        Map mapSur;
-
-        //member functions
-
-        void handleKeyboard(SDLKey key, bool keyDown);
 };
 
 typedef boost::shared_ptr<HumanView> HumanViewPtr;
