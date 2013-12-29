718bb606c61324dfcf6d34bc8ab0f73b4c82abd0
Brandon Haley <brandon.haley94@gmail.com>
2013-12-28 22:45:44 -0800

    Defined function for adding processes

--- src/ProcManager.cpp
+++ src/ProcManager.cpp
@@ -1,1 +1,21 @@
 #include "ProcManager.h"
+
+bool ProcManager::addProcess(ProcessPtr obj, std::string group = "NONE")
+{
+    uint32_t hashGroup = CRC32(group.c_str(), group.length());
+    ProcessMap::iterator it = processes.find(hashGroup);
+    ProcessList *cur;
+    if(it == std::map::end)
+    {
+        cur = new ProcessList();
+        ProcessEnt ent(hashGroup, cur);
+        ProcessRes res = processes.insert(ent);
+        Event_System::getSingleton().queueEvent(EventPtr(new MsgEvt("New list added for group named: " + group)));
+    }
+    if(obj.get() == NULL)
+    {
+        return false;
+    }
+    cur->push_back(obj);
+    return true;
+}
--- src/ProcManager.h
+++ src/ProcManager.h
@@ -2,6 +2,8 @@
 #include <string>
 #include "Methods.h"
 #include <map>
+#include <boost/shared_ptr.hpp>
+#include "Event_System.h"
 
 class Process;
 typedef boost::shared_ptr<Process> ProcessPtr;
@@ -9,24 +11,26 @@ typedef boost::shared_ptr<Process> ProcessPtr;
 class ProcManager
 {
     public:
+        
         bool addProcess(ProcessPtr obj, std::string group = "NONE");
         void pauseProcess(std::string name);
         void pauseAllProcesses();
         void pauseGroup(std::string groupName);
         void tick();
-        static ProcManager & getSingleton();
         
     private:
+        
         typedef std::vector<ProcessPtr> ProcessList;
         typedef std::map<uint32_t, ProcessList> ProcessMap;
         typedef std::pair<uint32_t, ProcessList> ProcessEnt;
         typedef std::pair<bool, ProcessMap::iterator> ProcessRes;
         
-        ProcessMap processes;
+        typedef std::map<uint32_t, bool> GroupMap;
+        typedef std::pair<uint32_t, bool> GroupEnt;
+        typedef std::pair<bool, GroupMap::iterator> GroupRes;
 
-        ProcManager(){}
-        ProcManager(ProcManager const &);
-        bool operator=(ProcManager const &);
+        ProcessMap processes;
+        GroupMap groups;
 };
 
 
