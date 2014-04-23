07f624dc66db2a746c65ffccb36d3b6d19cc0ddb
Brandon Haley <brandon.haley94@gmail.com>
2014-04-22 18:07:39 -0700

    Removed SDL

--- src/View.h
+++ src/View.h
@@ -10,11 +10,17 @@ class View
     public:
         View(uint32_t pid){id = pid;}
         virtual ~View(){}
+        // DataPacket passed to class, should not alter DataPacket
         virtual bool add(DataPacketPtr data)=0;
+        // Set up
         virtual void preFrame()=0;
+        // manipulation of the datapacket allowed
         virtual void onFrame(DataPacketPtr data)=0;
+        // clean up for the frame
         virtual void postFrame()=0;
+        // notification of deletion for a DataPacket
         virtual void remove(std::string name)=0;
+        // set up
         virtual bool init()=0;
         uint32_t getID(){return id;}
     protected:
